de3777baac85f903d06ba58b39d26c2c41ac9eb4
manjitha1216 <Manjitha-Punethri.Manchanayake@edu.rtu.lv>
2023-08-11 14:53:04 +0300

    adding pythontask1

---
+++ project1/pythontask1.py
@@ -0,0 +1,8 @@
+print("There was a man named John,")
+b = 'he was 60 years old'
+print(b)
+c = 'he really liked'
+d = 'his name John'
+result = "{} is {}".format(c,d)
+print(result)
+print('but didn\'t like being 60')
